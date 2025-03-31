6eb61ab55c96cf0e8518a03fcbec7c4122591b66
Kevin Cardenas <53354799+c14kevincardenas@users.noreply.github.com>
2025-03-31 09:22:56 -0600

    added path for limb xy location model

--- utils/detection/path_manager.py
+++ utils/detection/path_manager.py
@@ -7,6 +7,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[2]
 MODELS_DIR = PROJECT_ROOT / 'models'
 VITPOSE_MODEL_PATH = str(MODELS_DIR / 'vitpose-s-coco.pth')
 YOLO_MODEL_PATH = str(MODELS_DIR / 'yolov8s.pt')
+LIMB_LOC_MODEL_PATH = str(MODELS_DIR / 'limb_xy.keras')
 
 # print(PROJECT_ROOT)
 # print(MODELS_DIR)
