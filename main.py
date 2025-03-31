334f014e91a3cf9a8d6f3b9651f196e9ca6a52a1
Kevin Cardenas <53354799+c14kevincardenas@users.noreply.github.com>
2025-03-31 09:25:35 -0600

    added limb student model and limb xy dense neural net

--- main.py
+++ main.py
@@ -6,23 +6,30 @@ from utils.detection.hold_detection import detect_motion, detect_holds, draw_hol
 from utils.detection.pose_estimation import estimate_pose, calculate_ipd
 from utils.detection.movement_detection import MovementDetector
 from utils.prediction.move_prediction import predict_move
-from utils.prediction.limb_prediction.models import load_limb_models
+from utils.prediction.limb_prediction.models import load_limb_models, load_limb_models_student
 from utils.prediction.location_prediction.models import load_location_models
 from utils.prediction.command_translation import calculate_direction_and_distance
 from utils.prediction.call_prediction import TextToSpeech, call_prediction
 
-movement_detector = MovementDetector(movement_threshold=0.05, stability_threshold=10)
-previous_moves = PreviousMoves(save_dir='/training_images/saved_moves')
+movement_detector = MovementDetector(movement_threshold=0.085, stability_threshold=6)
+previous_moves = PreviousMoves(save_dir='C:/Users/Student/Desktop/beta_caller/training_images/saved_moves')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 detr_model_name = 'facebook/detr-resnet-50'
 img_model_name = 'c14kevincardenas/ClimBEiT_single_frame'
-climbeit_limb_model_name = 'c14kevincardenas/ClimBEiTv2'
+img_model_name_stud = 'microsoft/beit-base-patch16-224'
+climbeit_limb_model_name = 'c14kevincardenas/ClimBEiT-t2'
+# climbeit_limb_model_name = 'c14kevincardenas/ClimBEiT-student-t2'
 hands_loc_model_name = 'c14kevincardenas/limbxy_hands'
 feet_loc_model_name = 'c14kevincardenas/limbxy_feet'
-print('Loading Moved Prediction Models...')
-limb_models = load_limb_models(detr_model_name, img_model_name, climbeit_limb_model_name, device)
+if 'student' in climbeit_limb_model_name.split('-'):
+    print('Loading ClimBEiT Student Limb Prediction Model...')
+    limb_models = load_limb_models_student(detr_model_name, img_model_name, img_model_name_stud, climbeit_limb_model_name, device)
+else:
+    print('Loading ClimBEiT Limb Prediction Model...')
+    limb_models = load_limb_models(detr_model_name, img_model_name, climbeit_limb_model_name, device)
+print('Loading Limb Location Models...')
 location_models = load_location_models(img_model_name, hands_loc_model_name, feet_loc_model_name, device)
-tts = TextToSpeech(rate=150, voice_index=1)
+tts = TextToSpeech(rate=140, voice_index=1)
 
 
 def main(cam_num, draw_holds, draw_pose):
@@ -35,6 +42,7 @@ def main(cam_num, draw_holds, draw_pose):
     holds = None
     ipd_pixels = None
     target_hold = None
+    location = None
     run_hold_detection = True
     ipd_calc_mode = False
     run_beta_caller = False
@@ -95,7 +103,7 @@ def main(cam_num, draw_holds, draw_pose):
 
                 # make move prediction
                 start_time = time.perf_counter()
-                limb, location = predict_move(frame, previous_moves, limb_models, location_models, device, use_seq)
+                limb, location = predict_move(frame, previous_moves, kps, limb_models, location_models, device, use_seq)
                 total_time = time.perf_counter() - start_time
                 print(f"\tTotal Move Prediction Time = {total_time:.4f} seconds")
 
@@ -106,16 +114,16 @@ def main(cam_num, draw_holds, draw_pose):
                 # call move (transmit command to climber)
                 call_prediction(tts, limb, direction, distance)
 
-            # elif state == "moving":
-            #     print("Climber is moving...")
-            # elif state == "stopped":
-            #     print("Climber is stable, no movement detected.")
+            elif state == "moving":
+                print("Climber is moving...")
+            elif state == "stopped":
+                print("Climber is stable, no movement detected.")
 
             # check to continue drawing pose and/or holds on the wall
             if draw_pose:
                 frame = pose_frame
             if draw_holds:
-                frame = draw_hold_bbs(frame, holds, target_hold)
+                frame = draw_hold_bbs(frame, holds, target_hold, location)
 
         # display image
         cv2.imshow("Beta Caller", cv2.resize(frame, (800, 600)))
@@ -128,7 +136,7 @@ def main(cam_num, draw_holds, draw_pose):
             ipd_calc_mode = not ipd_calc_mode  # toggle ipd calc mode
             print("Calculate interpupillary distance mode:", "ON" if ipd_calc_mode else "OFF")
             if ipd_calc_mode:
-                print("\nClimber, move to the wall and face the camera. 'd': back to finding holds. 'h': draw holds. "
+                print("\nClimber, move to the wall and face the camera.\n'd': back to finding holds. 'h': draw holds. "
                       "'p': draw pose. 'ENTER': start Beta Caller.")
         elif key in [10, 13] and len(holds) > 0 and ipd_pixels is not None:  # pressed ENTER to start Beta Caller
             run_hold_detection = False
